0d773274fb89a7106126d436e69900ec8148d50d
Y_KUNIEDA <kyasuhiro9@gmail.com>
2024-03-15 17:32:27 +0900

    field updated

--- buffer_and_display/buffer/field.cpp
+++ buffer_and_display/buffer/field.cpp
@@ -16,10 +16,15 @@ field::field(){
     NUMBER_OF_COLUMN = setting.getCastedToIntSettingFromEntireSetting("NUMBER_OF_COLUMN");
     NUMBER_OF_LETTERS_IN_COLUMN = setting.getCastedToIntSettingFromEntireSetting("NUMBER_OF_LETTERS_IN_COLUMN");
     NUMBER_OF_SPACE_BETWEEN_COLUMNS = setting.getCastedToIntSettingFromEntireSetting("NUMBER_OF_SPACE_BETWEEN_COLUMNS");
+    IS_ZENKAKU_BASED = setting.getCastedToIntSettingFromEntireSetting("IS_ZENKAKU_BASED");
+    IS_SHOWING_COLUMN_NUMBER = setting.getCastedToIntSettingFromEntireSetting("IS_SHOWING_COLUMN_NUMBER");
+    IS_SHOWING_ROW_NUMBER = setting.getCastedToIntSettingFromEntireSetting("IS_SHOWING_ROW_NUMBER");
 
     for(size_t i=0;i<NUMBER_OF_ROW;i++){
         field_p[i] = new char[(NUMBER_OF_LETTERS_IN_COLUMN + NUMBER_OF_SPACE_BETWEEN_COLUMNS)*NUMBER_OF_COLUMN + 1];
     }
+
+    setting.releaseMemoryOfEntireSetting();
 }
 
 //destructor
@@ -27,3 +32,13 @@ field::~field() noexcept{
     delete field_p;
 }
 
+void field::displayField(){
+    char filling_letter;
+
+    if(IS_ZENKAKU_BASED){
+        filling_letter = '　';
+    } else {
+        filling_letter = ' ';
+    }
+
+}
--- buffer_and_display/buffer/field.hpp
+++ buffer_and_display/buffer/field.hpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <stdio.h>
 #include <string>
+#include <vector>
 
 class field{
     private:
@@ -11,13 +12,17 @@ class field{
     unsigned int NUMBER_OF_COLUMN;
     unsigned int NUMBER_OF_LETTERS_IN_COLUMN;
     unsigned int NUMBER_OF_SPACE_BETWEEN_COLUMNS;
+    bool IS_ZENKAKU_BASED;
+    bool IS_SHOWING_COLUMN_NUMBER;
+    bool IS_SHOWING_ROW_NUMBER;
 
     char** field_p = new char*[NUMBER_OF_ROW + 1];
+    std::vector<std::vector<std::string>> contents;
 
     public:
     field();
     ~field();
-    
+    void displayField();
 };
 
 #endif //FIELD_HPP
